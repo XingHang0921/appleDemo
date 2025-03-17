44ef9488ad6e1730103be608a7538ea6ad41eed1
xingh <nevinzheng0921@gmail.com>
2025-03-17 16:56:53 -0400

    procedding to highlight

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,6 +27,7 @@ const Hero = () => {
       opacity:1,
       delay:1.5,
     })
+    gsap.to('#cta',{ opacity:1, delay:2, y: -50})
   }, [])
 
   return (
@@ -39,6 +40,13 @@ const Hero = () => {
         </div>
       </div>
 
+      <div id='cta'
+            className="flex flex-col items-center opacity-0
+                      translate-y-20">
+        <a href="#highlights" className="btn">Buy</a>
+        <p className="font-normal text-xl">From 199/month or $999</p>
+        
+      </div>
     </section>
   )
 }
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,13 @@
 
 const Highlights = () => {
   return (
-    <div>Highlights</div>
+    <section id="highlights" className="w-screen"
+    overflow-hidden h-full common-padding bg-zinc>
+      <div className="screen-max-width">
+        <h1 id='title' ></h1>
+      </div>
+
+    </section>
   )
 }
 
