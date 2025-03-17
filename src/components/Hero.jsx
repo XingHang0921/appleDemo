9885711dda9fd9acb9cc762529814f594b3df343
Nevin <nevinzheng0921@gmail.com>
2025-03-17 13:29:59 -0400

    3/17 added video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,42 @@
-import gsap from "gsap"
-import { useGSAP } from "@gsap/react"
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import {heroVideo, smallHeroVideo } from '../utils';
+import { useEffect, useState } from "react";
 
 const Hero = () => {
-  // useGSAP(() =>{
-  //   gsap.to()
-  // }, [])
+  const [videoSrc, setVideoSrc] = useState(
+      window.innerWidth < 760 ? smallHeroVideo: heroVideo)
+
+  const handleVideoSrcSet = () => {
+    if(window.innerWidth < 760){
+      setVideoSrc(smallHeroVideo)
+    } else{
+      setVideoSrc(heroVideo)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize',handleVideoSrcSet);
+    
+    return()=>{
+      window.removeEventListener('resize', handleVideoSrcSet)
+    }
+  },[])
+  useGSAP(() =>{
+    gsap.to('#hero',{
+      opacity:1,
+      delay:1.5,
+    })
+  }, [])
 
   return (
     <section className="w-full nav-height bg-black relative">
       <div className=" h-5/6 w-full flex-center flex-col">
-        <p className="hero-title">iphone 15 Pro</p>
+        <p id='hero' className="hero-title">iPhone 15 Pro</p>
+        <div className="md:w-10/12 w-9/12">
+          <video autoPlay muted playsInline key={videoSrc}
+            src={videoSrc} type='video/mp4'></video>
+        </div>
       </div>
 
     </section>
