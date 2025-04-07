c4dcc50b33e6f986ff5e30ae4ff39ad158264470
xingh <nevinzheng0921@gmail.com>
2025-04-07 12:26:35 -0400

    change practice to my style, self learning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import { appleImg, bagImg, searchImg } from '../utils';
 import {navLists} from '../constants'
+
+const navImg = [bagImg, searchImg]
 const Navbar = () => {
   return (
     <header className='w-full py-5 sm:px-10 px:5 flex
-                     justify-between items-center'>
-        <nav className='flex w-full screen-max-width'>
+                       items-center'>
+        <nav className='flex w-full max-w-[1120px] mx-auto relative'>
             <img src={appleImg} alt="Apple" width={14} height={18}/>
             <div className='flex flex-1 justify-center max-sm:hidden'>
                 {navLists.map((nav) => (
@@ -13,12 +15,14 @@ const Navbar = () => {
                         {nav}
                     </div>
                 ))}
-                <div className='flex item-baseline gap-7 max-smLjustify-end 
-                                max-sm:flex-1'>
-                    <img src={searchImg} alt="search" width={18} height={18} />
-                    <img src={bagImg} alt="bag" width={18} height={18} />
-                </div>
             </div>
+            <div className='flex justify-end max-sm:flex-1'>
+                {navImg.map((img, index) =>(
+                    <img src={img} alt={img} width={14} height={18}
+                    className={index === navImg.length - 1? 'ml-10':''}/>
+                ))}
+            </div>
+            
         </nav>
     </header>
   )
